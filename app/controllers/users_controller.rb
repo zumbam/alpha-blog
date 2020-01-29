5e73a6a4b07ba488c39a443cfae9aa7555d470e8
zumbam <stefanronczka@outlook.de>
2020-01-29 16:28:37 +0100

    adding user editing and move account form into partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash['success'] = "Welcome to Alpha-Blog #{@user.username}"
+      flash[:success] = "Welcome to Alpha-Blog #{@user.username}"
       redirect_to articles_path
     else
       render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    successfully_updated = @user.update(user_params)
+    if successfully_updated
+      flash[:success] = 'Your accout has successfully updated'
+      redirect_to articles_path
+    else
+      render :edit
     end
   end
 
