57a47872abfb3bf881e4b1bfd0fcf8869e7d7844
zumbam <stefanronczka@outlook.de>
2020-01-29 18:58:36 +0100

    adding users index, style the user and article show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
 
   VALID_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
-  validates :email, presence: true, uniqueness: {case_sensitive: false}, length:{maximum: 50}, format: {with: VALID_FORMAT_REGEX}
+  validates :username, presence: true, uniqueness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 25}, format: {without: /\s/i}
+  validates :email, presence: true, uniqueness: {case_sensitive: false},
+            length:{maximum: 50}, format: {with: VALID_FORMAT_REGEX}
 end
