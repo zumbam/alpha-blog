c005ecc9cf1b25aa578ebbaec68af4eb2b246c30
zumbam <stefanronczka@outlook.de>
2020-01-29 13:25:18 +0100

    adding user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  
+
+  VALID_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+  validates :email, presence:true, uniqueness: {case_sensitive: false}, length:{maximum: 50}, format: {with: VALID_FORMAT_REGEX}
 end
