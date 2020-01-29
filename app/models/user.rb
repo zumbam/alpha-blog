926971666537e7f5a75ba0e7998f08bb65972fac
zumbam <stefanronczka@outlook.de>
2020-01-29 13:58:50 +0100

    adding user article association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :articles
+  # save the email in downcase
+  before_save { self.email = email.downcase }
 
   VALID_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
-  validates :email, presence:true, uniqueness: {case_sensitive: false}, length:{maximum: 50}, format: {with: VALID_FORMAT_REGEX}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, length:{maximum: 50}, format: {with: VALID_FORMAT_REGEX}
+  validates :user_id, presence: true
 end
