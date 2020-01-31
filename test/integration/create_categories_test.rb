c36ea100a41adf24ef142ed074ff161a20d9540e
zumbam <stefanronczka@outlook.de>
2020-01-31 15:10:52 +0100

    adding a test in failure case of category submission

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -15,4 +15,18 @@ class  CreateCategoriesTest < ActionDispatch::IntegrationTest
 
   end
 
+  test 'invalid category submission results in failure' do
+    get new_category_path
+    assert_response :success
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: {category: {name: ''}}
+    end
+    assert_template 'categories/new'
+    assert_select 'div.alert-danger'
+    assert_select 'h2.alert-heading'
+    assert_select 'div.alert-body'
+
+  end
+
 end
