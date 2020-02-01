3a8c47db8a8d13af56f6c7ec343fa6447a32ca88
zumbam <stefanronczka@outlook.de>
2020-02-01 11:31:29 +0100

    update view to pass categories to an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ private
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
